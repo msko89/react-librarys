54a94e7270b858800826692547bcb4de31c1fbb7
msko <kms89123@gmail.com>
2022-01-04 02:35:13 +0900

    Emotion Basic

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ npm i -D styled-components
 ```
 
 https://styled-components.com/docs/basics
+
+- ### **emotion**
+
+```properties
+npm i @emotion/styled @emotion/react
+```
+
+https://emotion.sh/docs/introduction
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import StyledComponent from './Style/StyledComponent/components/StyledComponent';
 import StyledComponentAdvanced from './Style/StyledComponent/components/StyledComponentAdvanced';
+import EmotionComponent from './Style/Emotion/components/EmotionComponent';
 
 function App() {
   return (
     <div className='App'>
       {/* <StyledComponent /> */}
-      <StyledComponentAdvanced />
+      {/* <StyledComponentAdvanced /> */}
+      <EmotionComponent />
     </div>
   );
 }
